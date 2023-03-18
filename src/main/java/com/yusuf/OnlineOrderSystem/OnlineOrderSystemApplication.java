459bc5c0a6b7f0f69938a7158ee9ed5735e64038
Yusuf Atalar <yusuf.atalar@iottech.com.tr>
2023-03-18 13:15:02 +0300

    Initial commit

---
+++ src/main/java/com/yusuf/OnlineOrderSystem/OnlineOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yusuf.OnlineOrderSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yusuf/OnlineOrderSystem/OnlineOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusuf.OnlineOrderSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
